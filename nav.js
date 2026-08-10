a414545f258019a3c3654d98cd3b8a8d3c182309
Pabsmophobia <pabsmophobia@gmail.com>
2026-08-10 01:48:23 +0100

    Fix duplicate entry for 'Contact' in nav.js

--- nav.js
+++ nav.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     { name: "Events", link: "events.html" },
     { name: "Blogs & Newsletters", link: "blog.html" },
     { name: "Evidence", link: "evidence.html" },
-    { name: "Contact", link: "contact.html" }
+    { name: "Contact", link: "contact.html" },
     { name: "Links", link: "links.html" }
   ];
 
